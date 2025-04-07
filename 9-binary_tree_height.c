55ab5edf38fe5b58475af4c000358394dc10b104
IEyZens <roncint61@gmail.com>
2025-04-07 12:01:42 +0200

    Fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,7 +12,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t right;
 
 	if (tree == NULL)
-		return (0 - 1);
+		return (0);
 
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
