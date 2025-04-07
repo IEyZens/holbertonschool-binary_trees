0ea2a61fef88498161b0aa305006d978eb75ba63
IEyZens <roncint61@gmail.com>
2025-04-07 12:16:22 +0200

    Add task 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ *
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth;
+
+	if (tree == NULL)
+		return (0);
+
+	return (binary_tree_depth(tree->parent) + 1);
+}
