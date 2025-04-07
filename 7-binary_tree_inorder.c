a24e61a0ed1914d507a37d67a840adaa856683c6
IEyZens <roncint61@gmail.com>
2025-04-07 11:08:25 +0200

    Add commentary

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,7 +1,9 @@
 #include "binary_trees.h"
 
 /**
- *
+ * binary_tree_preorder - check the code
+ * @tree: est un pointe vers une structure
+ * @func: est un pointeur de fonction
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
