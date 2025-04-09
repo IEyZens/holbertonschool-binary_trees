0e90495c381d1d40eec221dc36ca47f4e5915c06
IEyZens <roncint61@gmail.com>
2025-04-09 09:59:19 +0200

    Add commentary

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,7 +1,10 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_leaves - check the code
+ * @tree: est un pointeur constant vers une structure
  *
+ * Return: valid the code
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
