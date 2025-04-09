3ea122e893695c9d6822ba3f1441323410690fe2
IEyZens <roncint61@gmail.com>
2025-04-09 10:39:37 +0200

    Add task 15

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - check the code
+ * @tree: est un pointeur constant vers une structure
+ *
+ * Return: valid the code
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (1);
+
+	if ((tree->left == tree->right) || (tree->left && tree->right))
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	else
+		return (0);
+}
